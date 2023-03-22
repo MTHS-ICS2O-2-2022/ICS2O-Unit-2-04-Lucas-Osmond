6555d51a5d9c3a6e3618efbea6966054e26b7428
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-03-22 17:05:22 +0000

    discarded changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Osmond
+// Created on: March 2023
 // This file contains the JS functions for index.html
 
 'use strict'
 /**
- * This function calculates area and perimeter of rectangle.
+ * This function calculates area and perimeter of triangle.
  */
 function calculate () {
   // input
@@ -14,10 +14,8 @@ function calculate () {
   const height = parseInt(document.getElementById('height-of-triangle').value)
 
   // process
-  const area = base * height
-  const perimeter = 2 / (base + height)
+  const area = base * height / 2
 
   // output
   document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
-  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
 }
