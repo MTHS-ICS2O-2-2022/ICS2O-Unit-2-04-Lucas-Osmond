096fc3b1f37bd3e2a8f9a972df2cec47c346a6f5
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-03-22 16:57:32 +0000

    added my java script code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = base * height
+  const perimeter = 2 / (base + height)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
